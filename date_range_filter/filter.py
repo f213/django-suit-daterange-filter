9e3687eb7efd2fb884d1682f172acd6c0a2abc93
Ference van Munster <ference@munstermedia.nl>
2017-06-26 09:43:18 +0200

    Set SuitDateWidget not required. Changed __get_filterargs to filter only filled in dates and add validation per-field

--- date_range_filter/filter.py
+++ date_range_filter/filter.py
@@ -9,6 +9,7 @@ from suit.widgets import SuitDateWidget
 
 
 class DateRangeForm(forms.Form):
+
     def __init__(self, *args, **kwargs):
         """
         Automaticaly generate form fields with dynamic names based on the filtering field name
@@ -16,26 +17,30 @@ class DateRangeForm(forms.Form):
         self.field_name = kwargs.pop('field_name', 'date')
         super().__init__(*args, **kwargs)
 
-        self.fields['%s_start' % self.field_name] = forms.DateField(widget=SuitDateWidget, label=pgettext('date', 'From'))
-        self.fields['%s_end' % self.field_name] = forms.DateField(widget=SuitDateWidget, label=pgettext('date', 'To'))
+        self.fields['%s_start' % self.field_name] = forms.DateField(
+            widget=SuitDateWidget, label=pgettext('date', 'From'), required=False)
+        self.fields['%s_end' % self.field_name] = forms.DateField(
+            widget=SuitDateWidget, label=pgettext('date', 'To'), required=False)
 
     def start_date(self):
         if self.is_valid():
             start = self.cleaned_data.get('%s_start' % self.field_name)
-            start = datetime.combine(start, time.min)
+            if start:
+                start = datetime.combine(start, time.min)
 
-            if settings.USE_TZ:
-                return timezone.make_aware(start)
+                if settings.USE_TZ:
+                    return timezone.make_aware(start)
 
             return start
 
     def end_date(self):
         if self.is_valid():
             end = self.cleaned_data.get('%s_end' % self.field_name)
-            end = datetime.combine(end, time.max)
+            if end:
+                end = datetime.combine(end, time.max)
 
-            if settings.USE_TZ:
-                return timezone.make_aware(end)
+                if settings.USE_TZ:
+                    return timezone.make_aware(end)
 
             return end
 
@@ -68,15 +73,20 @@ class DateRangeFilter(admin.FieldListFilter):
         """
         self.form = form
 
-        if form.is_valid():
+        start_date = form.start_date()
+        end_date = form.end_date()
+
+        if form.is_valid() and (start_date or end_date):
             args = self.__get_filterargs(
-                start=form.start_date(),
-                end=form.end_date(),
+                start=start_date,
+                end=end_date,
             )
             return queryset.filter(**args)
 
     def __get_filterargs(self, start, end):
-        return {
-            self.field_path + '__gte': start,
-            self.field_path + '__lte': end,
-        }
+        filterargs = {}
+        if start:
+            filterargs[self.field_path + '__gte'] = start
+        if end:
+            filterargs[self.field_path + '__lte'] = end
+        return filterargs
