641d216af224435fb70faddaea031882b2d3a76c
Fedor Borshev <f@f213.in>
2018-06-13 13:21:39 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='django-suit-daterange-filter',
-    version='0.0.5',
+    version='0.0.6',
     description='Simplest possible date range filter for Django admin',
     packages=['date_range_filter'],
     install_requires=[
